e8dc38e78e498b7775a56b157f4b6a3510e0b1ac
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-06-09 16:26:37 +0300

    proje sonu

--- EnvironmentTranstion/Program.cs
+++ EnvironmentTranstion/Program.cs
@@ -1,16 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 
-var env = builder.Environment.EnvironmentName;
-Console.WriteLine(env);
+var env = builder.Environment.EnvironmentName; // Development , Production
 
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
-    .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: false);
+    //.AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: false)
+    ;
 
 var connectionString = builder.Configuration.GetSection("SqlConnection").Value;
 Console.WriteLine(connectionString);
+
+
+var app = builder.Build();
+
 
 app.MapGet("/", () => "Hello World!");
 
