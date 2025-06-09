8104564a7c6035e25d4fb8314226ba6ffc3a5cd8
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-06-09 14:59:18 +0300

    geliştirme ortamında istediğimiz ortama göre ayağa kaldırmak için
    dotnet run --launch-profile test/development/production diye ayarlarsak ona göre çalıştırabiliyoruz.

--- EnvironmentTranstion/Program.cs
+++ EnvironmentTranstion/Program.cs
@@ -1,8 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var connectionString = builder.Configuration.GetSection("SqlConnection");
 
+var env = builder.Environment.EnvironmentName;
+Console.WriteLine(env);
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+    .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: false);
+
+var connectionString = builder.Configuration.GetSection("SqlConnection").Value;
 Console.WriteLine(connectionString);
 
 app.MapGet("/", () => "Hello World!");
